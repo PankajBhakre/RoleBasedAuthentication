55360f9267223ef632767ef9f41664920bcfab20
Pankaj Bhakre <pankajbhakre11@gmail.com>
2021-07-21 20:33:45 +0530

    Commit Amplify for aws coding

---
+++ amplify-app/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'amplify-app';
+  authState;
+  user: CognitoUserInterface | undefined
+
+  constructor(private ref: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+    })
+  }
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
+
+}
